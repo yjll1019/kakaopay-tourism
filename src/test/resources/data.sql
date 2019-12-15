1f0132c72afd972a07d09b3c8b272ba04c590284
yjll1019 <yjll1019@naver.com>
2019-12-15 21:59:50 +0900

    [#16] chore: 테스트를 위한 데이터 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,9 +1,14 @@
 insert into region values('region02e22707', '강원도 속초');
+insert into region values('region02e21231', '강원도 양양');
 
 insert into theme(name) values('생태체험');
+insert into theme(name) values('문화체험');
 
 insert into program(contents, introduce, name) values ('강원도 여행', '강원도로 떠나는 여행', '강원도');
+insert into program(contents, introduce, name) values ('양양 여행', '양양으로 가는 여행', '양양');
 
 insert into program_regions values(1, 'region02e22707');
-
 insert into program_themes values(1, 1);
+
+insert into program_regions values(2, 'region02e21231');
+insert into program_themes values(2, 2);
